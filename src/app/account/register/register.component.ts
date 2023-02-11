de5ed4c574b01b8de68cc7d9b895d914fd8a7285
PonlawatP <ict45150@gmail.com>
2023-02-11 22:38:02 +0700

    member -> load data

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -27,7 +27,52 @@ export class RegisterComponent implements OnInit {
   zipcode = "";
   tel = "";
 
+  username_err = false;
+  name_err = false;
+  surname_err = false;
+  password_err = false;
+  conf_password_err = false;
+  address_err = false;
+  t_address_err = false;
+  a_address_err = false;
+  p_address_err = false;
+  zipcode_err = false;
+  tel_err = false;
+  // bg-rose-200 
+
   submit(){
+    this.username_err = this.username.trim() == ''
+    this.name_err = this.name.trim() == ''
+    this.surname_err = this.surname.trim() == ''
+    this.password_err = this.password.trim() == ''
+    this.conf_password_err = this.conf_password.trim() == ''
+    this.address_err = this.address.trim() == ''
+    this.t_address_err = this.t_address.trim() == ''
+    this.a_address_err = this.a_address.trim() == ''
+    this.p_address_err = this.p_address.trim() == ''
+    this.zipcode_err = this.zipcode.trim() == ''
+    this.tel_err = this.tel.trim() == ''
+    if(
+      this.username_err ||
+      this.name_err ||
+      this.surname_err ||
+      this.password_err ||
+      this.conf_password_err ||
+      this.address_err ||
+      this.t_address_err ||
+      this.a_address_err ||
+      this.p_address_err ||
+      this.zipcode_err ||
+      this.tel_err
+    ) {
+      this.toast.error("โปรดลองอีกครั้ง", 'กรอกข้อมูลไม่ครบ')
+      return
+    }
+    if(this.password.trim() != this.conf_password.trim()){
+      this.toast.error("โปรดลองอีกครั้ง", 'รหัสผ่านไม่ตรงกัน')
+      return
+    }
+
     let bd: any = {
       "username": this.username, 
       "name": this.name,
@@ -44,12 +89,13 @@ export class RegisterComponent implements OnInit {
     this.http.post('/api/customers', bd).subscribe({
       next: (data:any) => {
   
-        this.toast.success("เข้าสู่ระบบสำเร็จ")
-        this.router.navigateByUrl("/members")
+        this.toast.success("ลงทะเบียนสำเร็จ")
+        localStorage.removeItem('auth_data');
+        this.router.navigateByUrl("/account/login")
       },
       error: (error:any) => {
           console.error('There was an error!', error);
-          this.toast.error("โปรดลองอีกครั้ง", 'ชื่อหรือรหัสผ่านไม่ถูกต้อง')
+          this.toast.error("โปรดลองอีกครั้ง", 'เกิดข้อผิดพลาดระหว่างลงทะเบียน')
       }
     })
   }
--- src/app/home/members/members.component.ts
+++ src/app/home/members/members.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-members',
@@ -7,108 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MembersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toast : ToastrService, private http:HttpClient, private router:Router) { }
+  members: any = []
 
   ngOnInit(): void {
+    const opt = {
+        headers: {
+            Authorization: 'Bearer ' + (JSON.parse(localStorage.getItem('auth_data') || '{}').token || '')
+        }
+    }
+    this.http.get('/api/customers', opt).subscribe({
+        next: (data:any) => {
+            this.members = data
+        },
+        error: (error:any) => {
+            localStorage.removeItem('auth_data')
+            this.router.navigateByUrl("/account/login")
+        }
+      })
   }
 
-  members = [
-    {
-        username: "foo",
-        name: "ฟูจิ",
-        surname: "สิบาระ",
-        address: "69 หมู่ 18",
-        t_address: "หนองโก",
-        a_address: "กระนวน",
-        p_address: "ขอนแก่น",
-        zipcode: "40170",
-        tel: "0880870777",
-        dateofregis: "2023-02-04 10:45:21"
-    },
-    {
-        username: "boo",
-        name: "บาบิ",
-        surname: "ไลลา",
-        address: "69 หมู่ 18",
-        t_address: "หนองโก",
-        a_address: "กระนวน",
-        p_address: "ขอนแก่น",
-        zipcode: "40170",
-        tel: "098890045",
-        dateofregis: "2023-02-04 15:46:17"
-    },
-    {
-        username: "bee",
-        name: "กีกี้",
-        surname: "นานิกะ",
-        address: "69 หมู่ 18",
-        t_address: "หนองโก",
-        a_address: "กระนวน",
-        p_address: "ขอนแก่น",
-        zipcode: "40170",
-        tel: "0093322234",
-        dateofregis: "2023-02-06 08:33:25"
-    },
-    {
-        username: "bong",
-        name: "เนียงสรัน",
-        surname: "บองเอย",
-        address: "69 หมู่ 18",
-        t_address: "หนองโก",
-        a_address: "กระนวน",
-        p_address: "ขอนแก่น",
-        zipcode: "40170",
-        tel: "034377349",
-        dateofregis: "2023-02-06 08:36:33"
-    },
-    {
-        username: "bang",
-        name: "สรัน",
-        surname: "สะสมทรัพย์",
-        address: "69 หมู่ 18",
-        t_address: "หนองโก",
-        a_address: "กระนวน",
-        p_address: "ขอนแก่น",
-        zipcode: "40170",
-        tel: "034345673",
-        dateofregis: "2023-02-06 08:43:00"
-    },
-    {
-        username: "bang",
-        name: "สรัน",
-        surname: "สะสมทรัพย์",
-        address: "69 หมู่ 18",
-        t_address: "หนองโก",
-        a_address: "กระนวน",
-        p_address: "ขอนแก่น",
-        zipcode: "40170",
-        tel: "034345673",
-        dateofregis: "2023-02-10 19:47:06"
-    },
-    {
-      username: "bang",
-        name: "สรัน",
-        surname: "สะสมทรัพย์",
-        address: "69 หมู่ 18",
-        t_address: "หนองโก",
-        a_address: "กระนวน",
-        p_address: "ขอนแก่น",
-        zipcode: "40170",
-        tel: "034345673",
-        dateofregis: "2023-02-11 18:30:46"
-    },
-    {
-        username: "bang",
-        name: "สรัน",
-        surname: "สะสมทรัพย์",
-        address: "69 หมู่ 18",
-        t_address: "หนองโก",
-        a_address: "กระนวน",
-        p_address: "ขอนแก่น",
-        zipcode: "40170",
-        tel: "034345673",
-        dateofregis: "2023-02-11 20:31:23"
-    }
-]
-
+  logout():void {
+    localStorage.removeItem('auth_data')
+    this.router.navigateByUrl("/account/login")
+  }
 }
