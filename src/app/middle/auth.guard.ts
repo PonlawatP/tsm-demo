31fab49531adf015ca1fee3ad5e20febf39ce7c4
PonlawatP <ict45150@gmail.com>
2023-02-11 20:26:17 +0700

    authguard update & login resp

--- src/app/middle/auth.guard.ts
+++ src/app/middle/auth.guard.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
@@ -6,15 +7,25 @@ import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from
 export class AuthGuard implements CanActivate {
     constructor(
         private router: Router,
+        private http:HttpClient
     ) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         if (localStorage.getItem('auth_data')) {
-            // authorised so return true
-            return true;
+            const opts = {
+                headers: {
+                    'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('auth_data') || '{}')
+                }
+            }
+            this.http.get('http://tsmapi.suksan.group/customers', opts).subscribe({
+                next: (data:any)=>{
+                    return true;
+                }
+            })
         }
 
         // not logged in so redirect to login page with the return url
+        localStorage.removeItem('auth_data')
         this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url }});
         return false;
     }
